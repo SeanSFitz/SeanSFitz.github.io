2fefd874281b4259d462bc2610f8989d78121287
SeanSFitz <sean.s.fitz@gmail.com>
2017-07-05 17:27:36 +0000

    added contact icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,14 +4,17 @@ class Contact extends Component {
   render() {
     return (
           <div className="contact-line row">
-            <div className="visibile-md-block">
+            <div className="hidden-small">
               <span className="contact col-md-3"><em>{this.props.info.phone}</em></span>
               <span className="contact col-md-3"><em>{this.props.info.email.text}</em></span>
               <span className="contact col-md-3"><em>{this.props.info.linkedin.text}</em></span>
               <span className="contact col-md-3"><em>{this.props.info.github.text}</em></span>
             </div>
-            <div className="">
-              <span className="contact logo col-md-3"><img src="/build/images/Github-Logo.png" /></span>
+            <div className="show-small">
+              <span className="contact col-md-3">{this.props.info.phone}</span>
+              <span className="contact col-md-3"><img src="/build/images/Mail-Logo.png" /></span>
+              <span className="contact col-md-3"><img src="/build/images/LinkedIn-Logo.png" /></span>
+              <span className="contact col-md-3"><img src="/build/images/Github-Logo.png" /></span>
             </div>
           </div>
     );
