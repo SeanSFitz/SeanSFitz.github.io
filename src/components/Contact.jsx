23b4a3aadeff3d3923916b19ddc49530adae1cc2
SeanSFitz <sean.s.fitz@gmail.com>
2017-07-14 03:00:21 +0000

    media queries and links active

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,19 +9,19 @@ class Contact extends Component {
                 <em className="show-small">{this.props.info.phone}</em>
               </span>
               <span className="contact col span_1_of_4">
-                <a href="">
+                <a href="mailto:sean.s.fitz@gmail.com">
                   <em className="hidden-small">{this.props.info.email.text}</em>
                   <i className="fa fa-envelope-o fa-3x show-small" aria-hidden="true"></i>
                 </a>
               </span>
               <span className="contact col span_1_of_4">
-                <a href="">
+                <a href="https://www.linkedin.com/in/seansfitzpatrick/">
                   <em className="hidden-small">{this.props.info.linkedin.text}</em>
                   <i className="fa fa-linkedin-square fa-3x show-small" aria-hidden="true"></i>
                 </a>
               </span>
               <span className="contact col span_1_of_4">
-                <a href="">
+                <a href="https://github.com/SeanSFitz">
                   <em className="hidden-small">{this.props.info.github.text}</em>
                   <i className="fa fa-github fa-3x show-small" aria-hidden="true"></i>
                 </a>
